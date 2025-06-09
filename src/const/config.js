062150c38e9143d3a66748e9303670198edc1ee3
Mateo <mateobinance1@gmail.com>
2025-06-09 18:35:07 -0300

    fix v-0 .env

--- src/const/config.js
+++ src/const/config.js
@@ -3,7 +3,10 @@
 if (process.env.NODE_ENV === 'production') {
   config.port = process.env.PORT,
   config.conexionstring = process.env.CONEXION
-} 
+} else {
+  config.port = 3001,
+  config.conexionstring = 'mongodb+srv://mateobinance1:Kamalionica1423_@cluster0.acphn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+}
 
 
 export default config
