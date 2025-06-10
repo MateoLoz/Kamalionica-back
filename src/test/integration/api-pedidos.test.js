6ef8919c1ff2ce57b1f51203f36fdd0daf94a389
Mateo <mateobinance1@gmail.com>
2025-06-10 14:28:30 -0300

    fix test pedidos

--- src/test/integration/api-pedidos.test.js
+++ src/test/integration/api-pedidos.test.js
@@ -5,14 +5,14 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe("GET pedidos", async () => {
-it("se busca que al hacer una peticion get se obtengan todos los pedidos", async function() {
+it("se busca que al hacer una peticion get se obtengan todos los pedidos", async () => {
      const res = await request.execute(app).get('/api/pedidos');
     expect(res).to.have.status(200);
 })
 })
 
 describe("POST pedidos", async () => {
-it("se busca que al hacer una peticion post se inserte correctamente el pedido", async function() {
+it("se busca que al hacer una peticion post se inserte correctamente el pedido", async () => {
      const res = await request.execute(app).post('/api/pedidos').send({carrito:{
         items:[{
             titulo:"Producto Pipeline",
